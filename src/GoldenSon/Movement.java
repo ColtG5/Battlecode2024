bb142a90e5517a3b20c6b7d0668dd2d42acccd3d
BrettLyle11 <122420347+BrettLyle11@users.noreply.github.com>
2024-01-14 12:08:01 -0700

    Update Movement.java

--- src/GoldenSon/Movement.java
+++ src/GoldenSon/Movement.java
@@ -5,6 +5,7 @@ import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
@@ -29,6 +30,21 @@ public class Movement {
             rc.move(dir.rotateLeft());
             return true;
         } else if (rc.canMove(dir.rotateRight())) {
+            rc.move(dir.rotateRight());
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean smallMove(Direction dir, ArrayList<MapLocation> mapLocations) throws GameActionException {
+        if (rc.canMove(dir) && !mapLocations.contains(rc.getLocation().add(dir))) {
+            rc.move(dir);
+            return true;
+        } else if (rc.canMove(dir.rotateLeft()) && !mapLocations.contains(rc.getLocation().add(dir.rotateLeft()))) {
+            rc.move(dir.rotateLeft());
+            return true;
+        } else if (rc.canMove(dir.rotateRight()) && !mapLocations.contains(rc.getLocation().add(dir.rotateRight()))) {
             rc.move(dir.rotateRight());
             return true;
         }
@@ -67,7 +83,29 @@ public class Movement {
             }
         }
     }
-
+    public boolean tryNewDirection(Direction hype, ArrayList<MapLocation> mapLocations) throws GameActionException {
+        Direction ogDir = hype;
+        while (true) {
+            hype = rotateOnce(lefty, hype);
+            if(!mapLocations.contains(rc.getLocation().add(hype))){
+                if(smallMove(hype)) {
+                    return true;
+                }
+            }
+             else {
+                MapLocation potentialLocation = rc.getLocation().add(hype);
+                if (rc.onTheMap(potentialLocation)) MovementStack.push(hype);
+                else {
+                    MovementStack.clear();
+                    return false;
+                }
+            }
+            if(ogDir == hype) {
+                MovementStack.clear();
+                return false;
+            }
+        }
+    }
     /**
      * @param mapLocation Location to move to
      * @return failed to move or not
@@ -89,6 +127,40 @@ public class Movement {
             if (rc.canMove(topOfStack)){
                 Direction last = topOfStack;
                 while (rc.canMove(topOfStack)) {
+                    MovementStack.pop();
+                    last = topOfStack;
+                    if (MovementStack.empty()) break;
+                    topOfStack = MovementStack.peek();
+                }
+                rc.move(last);
+                return true;
+            }
+            else return tryNewDirection(topOfStack);
+        }
+    }
+
+    public boolean hardMove(MapLocation mapLocation, ArrayList<MapLocation> arrayList) throws GameActionException {
+        if (!rc.isMovementReady()) return false;
+
+        if (MovementStack.empty()) {
+            //HYPE is the direction
+            Direction hype = rc.getLocation().directionTo(mapLocation);
+            if(!arrayList.contains(rc.getLocation().add(hype))){
+                if (smallMove(hype, arrayList)) return true;
+                else {
+                    MovementStack.push(hype);
+                    return tryNewDirection(hype, arrayList);
+                }
+            }
+            else {
+                MovementStack.push(hype);
+                return tryNewDirection(hype, arrayList);
+            }
+        } else {
+            Direction topOfStack = MovementStack.peek();
+            if (rc.canMove(topOfStack) && !arrayList.contains(rc.getLocation().add(topOfStack))){
+                Direction last = topOfStack;
+                while (rc.canMove(topOfStack) &&!arrayList.contains(rc.getLocation().add(topOfStack))) {
                     MovementStack.pop();
                     last = topOfStack;
                     if (MovementStack.empty()) break;
